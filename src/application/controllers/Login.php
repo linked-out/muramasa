b353fb56b5baf84627229f65ffac268cb006336f
Ganesha Danu <ganeshadanu1@gmail.com>
2019-04-07 15:42:48 +0700

    add login view

--- src/application/controllers/Login.php
+++ src/application/controllers/Login.php
@@ -2,13 +2,13 @@
         
 defined('BASEPATH') OR exit('No direct script access allowed');
         
-class login extends CI_Controller {
+class Login extends CI_Controller {
 
-public function index()
-{
-                
-}
-        
+    public function index()
+    {
+        $this->load->view("login_view");                    
+    }
+            
 }
         
     /* End of file  login.php */
---
+++ src/application/views/login_view.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css"> 
+    <style>
+        * {
+            text-align: center;
+            color: #333;
+        } 
+
+        input, .input {
+            width: 300px;
+        }
+
+        .container {
+            margin-top: 100px;
+        }
+
+        .form {
+            margin: 20px;
+        }
+
+        footer {
+            border-top: solid #666 1px;
+            padding: 10px;
+            width: 100vw;
+            text-align: center;
+            position: absolute;
+            bottom: 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="container is-fluid">
+        <h2 class="title is-6">LinkedOut <i class="has-text-link fa-2x fab fa-linkedin"></i></h2>
+        <h3 class="title is-4">Welcome Back</h3>
+        <small>Don't miss your next opportunity. Sign in to stay updated on your professional world.</small>
+        <form action="" class="form">
+            <div class="field">
+                <input class="input is-medium" type="email" placeholder="Enter your email">
+            </div>
+            <div class="field">
+                <input class="input is-medium" type="password" placeholder="Enter your password">
+            </div>
+            <div class="field">
+                <input class="is-link button is-medium" type="submit" value="Sign In">
+            </div>
+        </form>
+        <a href="" class="is-link">Forgot Password?</a><br>
+        <span>New to linkedOut? <a href="" class="is-link">Join Now</a></span>
+    </div>
+
+    <footer>
+            <small>Aplikasi ini dikerjakan sendiri oleh <i class="fab fa-github"></i>  <a href="https://github.com/blinfoldking" class="is-link">blinfoldking</a> </small>
+    </footer>
+</body>
+</html>
