7dba6649521560438e5e94bd5a8585933f60c020
Ganesha Danu <ganeshadanu1@gmail.com>
2019-04-11 03:37:51 +0700

    add profile controller

---
+++ src/application/controllers/Profile.php
@@ -0,0 +1,16 @@
+<?php 
+        
+defined('BASEPATH') OR exit('No direct script access allowed');
+        
+class Profile extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->view('navigation_bar');                
+    }
+        
+}
+        
+    /* End of file  Profile.php */
+        
+                            
