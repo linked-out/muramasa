0f5aa5e4d76e40afb68bfe0fc9859e57a7c89ad8
Ganesha Danu <ganeshadanu1@gmail.com>
2019-04-23 00:27:15 +0700

    restructuring folder

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM linode/lamp
-COPY run.sh /run.sh
-COPY src/ /var/www/example.com/public_html/
-RUN chmod +x /run.sh
-CMD ["/bin/bash", "-lc", "/run.sh"]
-
-
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-service apache2 start
-service mysql start
-rm /var/www/example.com/public_html/index.html
-echo "Services started"
-tail -f /dev/null
