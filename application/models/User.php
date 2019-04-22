c04cc74801612f4c5d67acaa2cc9198aad23dc75
Ganesha Danu <ganeshadanu1@gmail.com>
2019-04-23 02:08:54 +0700

    refactor user model

--- application/models/User.php
+++ application/models/User.php
@@ -3,10 +3,19 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
                         
 class User extends CI_Model {
-                        
-    public function new($user){
+    public function __construct()
+    {
+        parent::__construct();
         $this->load->database();
+    }                   
+    
+
+    public function new($user){
         $this->db->insert('USER', $user); 
+    }
+
+    public function login($username, $password) {
+
     }
 }
                         
