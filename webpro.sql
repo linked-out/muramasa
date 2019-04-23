a881c50980476830db16a3ef370ffea79168c33c
Ganesha Danu <ganeshadanu1@gmail.com>
2019-04-23 19:56:59 +0700

    add sql script

---
+++ webpro.sql
@@ -0,0 +1,30 @@
+CREATE TABLE USER(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    PASSWORD VARCHAR(255)
+);
+
+CREATE TABLE profile(
+   id INT PRIMARY KEY,
+   motto VARCHAR(255),
+   city VARCHAR(255),
+   state VARCHAR(255),
+   FOREIGN KEY (id) REFERENCES USER(id)
+);
+
+CREATE TABLE post(
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   content VARCHAR(255),
+   user_id INT,
+   FOREIGN KEY (user_id) REFERENCES USER(id)
+);
+
+CREATE TABLE job (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   title VARCHAR(255),
+   company_name VARCHAR(255),
+   user_id INT,
+   FOREIGN KEY (user_id) REFERENCES USER(id)
+);
