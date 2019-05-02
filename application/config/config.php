48781ffa45e953bf7d88395f768775a02f974293
Ganesha Danu <ganeshadanu1@gmail.com>
2019-05-02 11:01:29 +0700

    change base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/xampp/muramasa/index.php';
+$config['base_url'] = 'https://muramasa-danu.herokuapp.com/index.php';
 
 /*
 |--------------------------------------------------------------------------
