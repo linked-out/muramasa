4a4847c2f55ffdb749471c77051b518a9d901784
Ganesha Danu <ganeshadanu1@gmail.com>
2019-05-02 11:25:36 +0700

    add session table

--- webpro.sql
+++ webpro.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+        `id` varchar(40) NOT NULL,
+        `ip_address` varchar(45) NOT NULL,
+        `timestamp` int(10) unsigned DEFAULT 0 NOT NULL,
+        `data` blob NOT NULL,
+        PRIMARY KEY (id),
+        KEY `ci_sessions_timestamp` (`timestamp`)
+);
+
 CREATE TABLE USER(
     id INT AUTO_INCREMENT PRIMARY KEY,
     firstname VARCHAR(255),
